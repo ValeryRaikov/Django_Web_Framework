8a2d92e2761bdb080942c21990fe39faea3e250d
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-05-02 10:33:56 +0300

    Delete models_in_django directory

--- models_in_django/urls_and_views/employees/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from urls_and_views.employees.models import Employee
-
-
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'job_title', 'company',]
